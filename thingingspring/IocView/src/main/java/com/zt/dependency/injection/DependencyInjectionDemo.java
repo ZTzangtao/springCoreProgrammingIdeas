1b8258c469eee17390cb83a3fb16613a732d7492
zangtao <zangtao3211@163.com>
2020-07-03 22:41:57 +0800

    依赖来源  2020 07 03

--- thingingspring/IocView/src/main/java/com/zt/dependency/injection/DependencyInjectionDemo.java
+++ thingingspring/IocView/src/main/java/com/zt/dependency/injection/DependencyInjectionDemo.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
@@ -16,9 +17,10 @@ public class DependencyInjectionDemo {
         //配置xml文件，
         //启动spring应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lnjection-context.xml");
+        //依赖来源一： 自定义的bean
         UserRepository user = (UserRepository) beanFactory.getBean("userRepository");
 //        System.out.println("实时"+user);
-        //依赖注入
+        //依赖来源二： 依赖注入（内建依赖）
         System.out.println(user.getBeanFactory());
 //        System.out.println(user.getBeanFactory() == beanFactory);
 
@@ -27,6 +29,9 @@ public class DependencyInjectionDemo {
         ObjectFactory objectFactory = user.getObjectFactory();
         System.out.println(objectFactory.getObject() == beanFactory);
 
+        //依赖来源三： 容器内建 Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println("获取Environment类型的bean："+environment);
     }
 
 }
