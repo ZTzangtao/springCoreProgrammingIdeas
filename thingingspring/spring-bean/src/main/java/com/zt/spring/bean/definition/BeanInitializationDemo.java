a4c99d4cdae0810ade4e7807116ca188a6e6fdf9
zangtao <zangtao3211@163.com>
2020-07-12 21:31:01 +0800

    Bean destroy 3 method 2020 07 12

--- thingingspring/spring-bean/src/main/java/com/zt/spring/bean/definition/BeanInitializationDemo.java
+++ thingingspring/spring-bean/src/main/java/com/zt/spring/bean/definition/BeanInitializationDemo.java
@@ -5,6 +5,7 @@ import com.zt.spring.bean.factory.UserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Bean 初始化 demo
@@ -23,16 +24,25 @@ public class BeanInitializationDemo {
         //启动spring应用上下文
         applicationContext.refresh();
 
+        //非延迟初始化在Spring应用上下文启动完成后，被初始化
+        System.out.println("Spring应用上下文已启动...");
+
         //依赖查找UserFactory
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        System.out.println(userFactory);
+
+        System.out.println("Spring应用上下文 准备关闭...");
 
         //关闭spring应用上下文
         applicationContext.close();
+        System.out.println("Spring应用上下文 已关闭...");
     }
 
-    @Bean(initMethod = "initUserFactory")
+    @Bean(initMethod = "initUserFactory",destroyMethod = "doDestroy")
+    @Lazy(value = true)
     public UserFactory userFactory(){
         return  new DefaultUserFactory();
     }
+
 
 }
--- thingingspring/spring-bean/src/main/java/com/zt/spring/bean/factory/DefaultUserFactory.java
+++ thingingspring/spring-bean/src/main/java/com/zt/spring/bean/factory/DefaultUserFactory.java
@@ -1,8 +1,10 @@
 package com.zt.spring.bean.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * 默认 {@link UserFactory}实现
@@ -10,7 +12,7 @@ import javax.annotation.PostConstruct;
  * @author Tommy
  * @date 2020/7/10 9:37 下午
  */
-public class DefaultUserFactory implements UserFactory, InitializingBean {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
 
     //1、基于@PostConstruct 注解
     @PostConstruct
@@ -32,4 +34,17 @@ public class DefaultUserFactory implements UserFactory, InitializingBean {
         System.out.println("多帅啊！");
     }
 
+    @PreDestroy
+    public void PreDestroy(){
+        System.out.println("@PreDestroy : UserFactory销毁中...");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean # destroy() : UserFactory销毁中...");
+    }
+
+    public void doDestroy(){
+        System.out.println("自定义销毁方法 doDestroy() : UserFactory销毁中...");
+    }
 }
