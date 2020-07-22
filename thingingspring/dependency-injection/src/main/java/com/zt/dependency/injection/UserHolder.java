8b1df7cdf4dcafe4bc1e81f93939d1979ae715a4
zangtao <zangtao3211@163.com>
2020-07-22 22:40:50 +0800

    dependency injection setter 2020 07 22 22:40

---
+++ thingingspring/dependency-injection/src/main/java/com/zt/dependency/injection/UserHolder.java
@@ -0,0 +1,15 @@
+package com.zt.dependency.injection;
+
+import com.zt.dependency.domain.User;
+import lombok.Data;
+
+/**
+ * {@link User}的Holder对象
+ *
+ * @author Tommy
+ * @date 2020/7/22 10:18 下午
+ */
+@Data
+public class UserHolder {
+    private User user;
+}
