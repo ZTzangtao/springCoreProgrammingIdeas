8aab91bf2dadf35c51d3901cff800cae8dc14af3
Tommy <zangtao3211@163.com>
2021-07-08 23:38:44 +0800

    Spring Boot 新建MessageSource Bean

---
+++ thingingspring/i18n/src/main/java/com/zt/dependency/spring/i18n/CustomizedMessageSourceBeanDemo.java
@@ -0,0 +1,54 @@
+﻿package com.zt.dependency.spring.i18n;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Spring Boot 场景下自定义 {@link MessageSource} Bean
+ * @Author: Tommy
+ * @DATE: 2021/7/8
+ *
+ * @see MessageSourceAutoConfiguration
+ * @see ReloadableResourceBundleMessageSource
+ */
+@EnableAutoConfiguration
+public class CustomizedMessageSourceBeanDemo { //@Configuration Class
+
+    /**
+     * 在Spring Boot场景中，Primary Configuration Sources(Classes)高于 *AutoConfiguration
+     *
+     * @return
+     */
+    @Bean(AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
+    public MessageSource messageSource() {
+        return new ReloadableResourceBundleMessageSource();
+    }
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(CustomizedMessageSourceBeanDemo.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+
+        if(beanFactory.containsBean(AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)) {
+            //查找MessageSource 的 BeanDefinition
+            System.out.println(beanFactory.getBeanDefinition(AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME));
+            //查找MessageSource bean
+            MessageSource messageSource = applicationContext.getBean(AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME,MessageSource.class);
+            System.out.println(messageSource);
+        }
+        //关闭应用上下文
+        applicationContext.close();
+    }
+}
