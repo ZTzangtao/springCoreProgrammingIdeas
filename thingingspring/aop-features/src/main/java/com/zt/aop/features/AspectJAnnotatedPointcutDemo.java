f1f5cb634306893df20f3ed00b8b5b558959cc37
Tommy <zangtao3211@163.com>
2021-12-06 23:16:59 +0800

    Join Point

---
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/AspectJAnnotatedPointcutDemo.java
@@ -0,0 +1,33 @@
+package com.zt.aop.features;
+
+import com.zt.aop.features.aspect.AspectConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Pointcut 示例
+ *
+ * @Author Tommy
+ * @Date 2021/11/29 9:43 PM
+ * @Version 1.0
+ */
+@Configuration // Configuration class
+@EnableAspectJAutoProxy // 激活 Aspect 注解自动代理
+public class AspectJAnnotatedPointcutDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AspectJAnnotatedPointcutDemo.class, AspectConfiguration.class);
+        context.refresh();
+
+        AspectJAnnotatedPointcutDemo aspectJAnnotationDemo = context.getBean(AspectJAnnotatedPointcutDemo.class);
+        aspectJAnnotationDemo.execute();
+        context.close();
+    }
+
+    public void execute() {
+        System.out.println("execute()...");
+    }
+
+}
--- thingingspring/aop-features/src/main/java/com/zt/aop/features/aspect/AspectConfiguration.java
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/aspect/AspectConfiguration.java
@@ -1,6 +1,8 @@
 package com.zt.aop.features.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Aspect 配置类
@@ -11,5 +13,15 @@ import org.aspectj.lang.annotation.Aspect;
  */
 @Aspect
 public class AspectConfiguration {
+
+    @Pointcut("execution(public * *(..))") // 匹配 Join Point
+    private void anyPublicMethod(){ // 方法名即 Pointcut 名
+        System.out.println("@Pointcut at any public method.");
+    }
+
+    @Before("anyPublicMethod()") // Join Point 拦截动作
+    public void beforeAnyPublicMethod() {
+        System.out.println("@Before any public method.");
+    }
 
 }
