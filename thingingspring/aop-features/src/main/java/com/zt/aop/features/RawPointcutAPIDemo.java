29ef41aaec772bb5c0cdda7974d237f58b935c56
Tommy <zangtao3211@163.com>
2021-12-11 12:06:11 +0800

    API实现 Join Point

---
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/RawPointcutAPIDemo.java
@@ -0,0 +1,35 @@
+package com.zt.aop.features;
+
+import com.zt.aop.features.interceptor.EchoServiceMethodInterceptor;
+import com.zt.aop.features.pointcut.EchoServicePointcut;
+import com.zt.aop.overview.DefaultEchoService;
+import com.zt.aop.overview.EchoService;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+/**
+ * @Author: Tommy
+ * @DATE: 2021/12/11
+ */
+public class RawPointcutAPIDemo {
+
+    public static void main(String[] args) {
+        EchoServicePointcut pointcut = new EchoServicePointcut("echo", EchoService.class);
+
+        // 将 Pointcut 适配成 Advisor
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new EchoServiceMethodInterceptor());
+
+        DefaultEchoService defaultEchoService = new DefaultEchoService();
+        ProxyFactory proxyFactory = new ProxyFactory(defaultEchoService);
+
+        // 添加 Advisor
+        proxyFactory.addAdvisor(advisor);
+
+        // 获取代理对象
+        EchoService echoService = (EchoService) proxyFactory.getProxy();
+        System.out.println(echoService.echo("Hello,World"));
+
+    }
+
+}
---
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/pointcut/EchoServicePointcut.java
@@ -0,0 +1,46 @@
+package com.zt.aop.features.pointcut;
+
+import org.springframework.aop.support.StaticMethodMatcherPointcut;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+/**
+ * @Author: Tommy
+ * @DATE: 2021/12/11
+ */
+public class EchoServicePointcut extends StaticMethodMatcherPointcut {
+
+    private String methodName;
+
+    private Class targetClass;
+
+    @Override
+    public boolean matches(Method method, Class<?> targetClass) {
+        return Objects.equals(methodName, method.getName())
+                && this.targetClass.isAssignableFrom(targetClass);
+    }
+
+    public EchoServicePointcut(String methodName, Class targetClass) {
+        this.methodName = methodName;
+        this.targetClass = targetClass;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Class getTargetClass() {
+        return targetClass;
+    }
+
+    public void setTargetClass(Class targetClass) {
+        this.targetClass = targetClass;
+    }
+
+
+}
