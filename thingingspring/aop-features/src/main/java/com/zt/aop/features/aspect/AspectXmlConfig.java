5cd9e4605ff8b1228cb21c576b0f0065aa262b2e
Tommy <zangtao3211@163.com>
2021-12-12 19:41:32 +0800

    xml 配置 after advice

--- thingingspring/aop-features/src/main/java/com/zt/aop/features/aspect/AspectXmlConfig.java
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/aspect/AspectXmlConfig.java
@@ -1,6 +1,11 @@
 package com.zt.aop.features.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+
+import java.util.Random;
 
 /**
  * Aspect 配置类
@@ -20,8 +25,27 @@ public class AspectXmlConfig {
     }
 
     public Object aroundAnyPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        Random random = new Random();
+
+        if(random.nextBoolean()){
+            throw new RuntimeException("For Purpose from XML configuration.");
+        }
+
         System.out.println("@Around any public method." + pjp.getSignature());
+
         return pjp.proceed();
+    }
+
+    public void finalizeAnyPublicMethod() {
+        System.out.println("@After any public method.");
+    }
+
+    public void afterAnyPublicMethod() {
+        System.out.println("@AfterReturning any public method.");
+    }
+
+    public void afterThrowingAnyPublicMethod() {
+        System.out.println("@AfterThrowing any public method.");
     }
 
 }
