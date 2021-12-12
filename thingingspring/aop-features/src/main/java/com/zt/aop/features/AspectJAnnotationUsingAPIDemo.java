e1e97c91c06df2d8937de47776020a877ec63584
Tommy <zangtao3211@163.com>
2021-12-12 19:55:08 +0800

    API 实现 三种 After Advice

--- thingingspring/aop-features/src/main/java/com/zt/aop/features/AspectJAnnotationUsingAPIDemo.java
+++ thingingspring/aop-features/src/main/java/com/zt/aop/features/AspectJAnnotationUsingAPIDemo.java
@@ -1,8 +1,10 @@
 package com.zt.aop.features;
 
 import com.zt.aop.features.aspect.AspectConfiguration;
+import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+import org.springframework.aop.framework.ProxyFactory;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -31,20 +33,29 @@ public class AspectJAnnotationUsingAPIDemo {
             @Override
             public void before(Method method, Object[] args, Object target) throws Throwable {
                 if("put".equals(method.getName()) && args.length == 2){
-                    System.out.printf("当前存放是 Key : %s , Value : %s \n", args[0], args[1]);
+                    System.out.printf("[MethodBeforeAdvice] 当前存放是 Key : %s , Value : %s \n", args[0], args[1]);
                 }
             }
         });
 
+        // 添加 AfterReturningAdvice
+        proxyFactory.addAdvice(new AfterReturningAdvice() {
+            @Override
+            public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+                if("put".equals(method.getName()) && args.length == 2){
+                    System.out.printf("[AfterReturningAdvice] 当前存放是 Key : %s , 新存放的 Value : %s , 之前关联的 Value : %s\n ",
+                            args[0],    // key
+                            args[1],    // new Value
+                            returnValue // old value
+                    );
+                }
+            }
+        });
 //        cache.put("1", "A");
         // 通过代理对象存储
         Map<String, Object> proxy = proxyFactory.getProxy();
         proxy.put("1", "A");
-
-
         System.out.println(cache.get("1"));
-
-
     }
 
 }
